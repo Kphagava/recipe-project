106aca89f3b39f339ba7f558bc4f2685774f7ab5
Keti Phagava <ketiphagava@Ketis-MacBook-Air.local>
2023-03-11 11:04:14 +0400

    Added Inputs

--- src/app/quick-info/quick-info.component.ts
+++ src/app/quick-info/quick-info.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-quick-info',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./quick-info.component.css']
 })
 export class QuickInfoComponent {
-
+@Input() recipeDifficulty: string = "unkown difficulty"
 }
--- src/app/recipe-details/recipe-details.component.ts
+++ src/app/recipe-details/recipe-details.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-recipe-details',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-details.component.css']
 })
 export class RecipeDetailsComponent {
-
+  @Input() recipeTitle: string = "unknown recipe"
+  @Input() recipeAuthor: string = "unknown author"
+  @Input() recipeDescription: string = "no description"
 }
